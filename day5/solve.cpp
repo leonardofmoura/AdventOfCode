d5620cfdfa700a302b89b49dc9c641d20e4b9561
Leonardo Moura <leofmoura17@gmail.com>
2020-12-06 20:26:26 +0000

    Added day 5

---
+++ day5/solve.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+std::vector<std::string> read_strings(const std::string filename) {
+    std::fstream file;
+    file.open(filename,std::ios::in);
+
+    if (!file.is_open()) {
+        return {};
+    }
+
+    std::string str;
+    std::vector<std::string> res;
+
+    //read the file
+    while (std::getline(file, str)) {
+        res.push_back(str);
+    }
+
+    file.close();
+
+    return res;
+}
+
+int get_seat_id(const std::string seat) {
+    if (seat.length() != 10) {
+        return -1;
+    }
+
+    double low = 0, high = 127;
+    double row, column;
+
+    for (int i = 0; i < 6; i++) {
+        if (seat[i] == 'F') {
+            high = floor(high - (high - low)/2);
+        }
+        else if (seat[i] == 'B') {
+            low = ceil(low + (high - low)/2);
+        }
+    }
+
+    if (seat[6] == 'F') {
+        row = low;
+    }
+    else if (seat[6] == 'B') {
+        row = high;
+    }
+
+    low = 0, high = 7;
+    for (int i = 7; i < 9; i++) {
+        if (seat[i] == 'L') {
+            high = floor(high - (high - low)/2);
+        }
+        else if (seat[i] == 'R') {
+            low = ceil(low + (high - low)/2);
+        }
+    }
+
+    if (seat[9] == 'L') {
+        column = low;
+    }
+    else if (seat[9] == 'R') {
+        column = high;
+    }
+
+    return row * 8 + column;
+}
+
+int main() {
+    std::vector<std::string> seats = read_strings("seats.txt");
+    std::vector<int> ids;
+
+    for (auto seat : seats) {
+        ids.push_back(get_seat_id(seat));
+    }
+
+    std::sort(ids.begin(),ids.end());
+
+    std::cout << "Highest id: " << ids[ids.size()-1] << std::endl;
+
+    for (int i = 0; i < ids.size()-1; i++) {
+        if (ids[i]+1 != ids[i+1]) {
+            std::cout << "Seat found! ID: " << ids[i]+1 << std::endl;
+        }
+    }
+
+    return 0;
+}
