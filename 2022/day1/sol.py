04f1da678d472057b0016c820af73524c5b62fbe
leonardofmoura <leofmoura17@gmail.com>
2022-12-24 13:56:00 +0000

    added day 1

--- 2022/day1/sol.py
+++ 2022/day1/sol.py
@@ -1,5 +1,21 @@
 from lib import *
 
 def solve():
-    a()
-    print("day 1 solved")
+    lines = readfile('day1')
+
+    gt = []
+    s = 0
+
+    for l in lines:
+        if (l == '\n'):
+            gt.append(s) 
+
+            s = 0
+            continue
+
+        s += int(l[:-1])
+
+        gt.sort(reverse=True)
+
+    print(f"Solution 1: {gt[0]}")
+    print(f"Solution 2: {gt[0] +  gt[1] + gt[2]}")
--- 2022/lib.py
+++ 2022/lib.py
@@ -1,2 +1,6 @@
-def a():
-    print("ola")
+
+def readfile(day):
+    d = f"{day}/in.txt"
+    with open(d, 'r') as file:
+        return file.readlines()
+    
